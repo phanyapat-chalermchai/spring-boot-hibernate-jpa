757086aadeb74832cab67a4400b4f651fa534353
Phanyapat Chalermchai <81134953+phanyapat-chalermchai@users.noreply.github.com>
2024-04-07 18:29:27 +0700

    add datasource

---
+++ crudProject/src/main/java/com/phanyapat/crudProject/CrudProjectApplication.java
@@ -0,0 +1,22 @@
+package com.phanyapat.crudProject;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CrudProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudProjectApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
+}
